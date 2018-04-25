076b4f87a39f7d9ca56c7e98e3c1d915495be006
TheNecromant <38696571+TheNecromant@users.noreply.github.com>
2018-04-26 00:11:45 +0500

    SDELAL

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -24,7 +24,7 @@ class m130524_201442_init extends Migration
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
             'date' => $this->date("Y-m-d"),
-            'phone' => $this->integer(),
+            'phone' => $this->text(),
             'family' => $this->integer()->defaultValue(Null),
             'children' => $this->integer()->defaultValue(Null),
         ], $tableOptions);
