707b3ef61ee95f37420c716226230e1e8f1dc6e3
TheNecromant <38696571+TheNecromant@users.noreply.github.com>
2018-05-08 00:43:59 +0500

    123

--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,15 @@
+<<<<<<< current
 <?php
 return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
 ];
+=======
+<?php
+return [
+    'adminEmail' => 'admin@example.com',
+    'supportEmail' => 'support@example.com',
+    'user.passwordResetTokenExpire' => 3600,
+  ];
+>>>>>>> before discard
