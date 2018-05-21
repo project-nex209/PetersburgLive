94892edcfb217fcfb14d27cbce4d748dabc1599a
TheNecromant <38696571+TheNecromant@users.noreply.github.com>
2018-05-21 17:15:29 +0500

    USER CRUD

---
+++ frontend/views/User/index.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Users';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create User', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'username',
+            'auth_key',
+            'password_hash',
+            'password_reset_token',
+            //'email:email',
+            //'status',
+            //'created_at',
+            //'updated_at',
+            //'date',
+            //'phone',
+            //'family',
+            //'children',
+            //'isAdmin',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
