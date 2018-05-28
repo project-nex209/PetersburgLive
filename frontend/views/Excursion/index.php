fd438cfb58f5c17450a2412a1fe45caf9d747920
TheNecromant <38696571+TheNecromant@users.noreply.github.com>
2018-05-28 19:04:24 +0500

    12313

--- frontend/views/Excursion/index.php
+++ frontend/views/Excursion/index.php
@@ -6,26 +6,29 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Экскурсии';
+$this->title = 'Excursions';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-
 <div class="excursion-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <p>
+        <?= Html::a('Create Excursion', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'columns' => [
-            //['class' => 'yii\grid\SerialColumn'],
+            ['class' => 'yii\grid\SerialColumn'],
 
-            //'id',
+            'id',
             'excursion',
             'position',
             'priceMan',
             'priceChildren',
 
-            ['class' => 'yii\grid\ActionColumn', 'template' => '{view}'],
+            ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
 </div>
--- frontend/views/token/create.php
+++ frontend/views/token/create.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Token */
 
-$this->title = 'Create Token';
+$this->title = 'Оформить билет';
 $this->params['breadcrumbs'][] = ['label' => 'Tokens', 'url' => ['index']];
 
 ?>
